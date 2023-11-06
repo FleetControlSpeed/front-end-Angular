f388cac5aade9856cf75dcbf256e684de72106cb
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-11-06 10:55:47 -0300

    listando multas

--- src/app/pages/listar-multas/listar-multas.component.ts
+++ src/app/pages/listar-multas/listar-multas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Multa } from 'src/app/model/Multa';
+import { MultaService } from 'src/app/service/Multa/multa.service';
 
 @Component({
   selector: 'app-listar-multas',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-multas.component.css']
 })
 export class ListarMultasComponent {
+  multas:Multa[] = [];
+
+  constructor(private multaService : MultaService){
+    this.listaMultas();
+  }
+  listaMultas(){
+    this.multaService.listar().subscribe(
+      (lista)=>{
+        this.multas = lista;
+      },
+      (erro)=>{
+        alert('Erro Inesperado');
+        console.error(erro);
+      }
+    )
+  }
 
 }
