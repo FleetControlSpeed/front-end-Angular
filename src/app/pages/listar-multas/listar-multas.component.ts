37d70a8cafc84074fb76aed4fb0f4a958227d807
Ademar <ademar.ramalho@hotmail.com>
2023-11-06 21:06:46 -0300

    editar veiculo

--- src/app/pages/listar-multas/listar-multas.component.ts
+++ src/app/pages/listar-multas/listar-multas.component.ts
@@ -11,6 +11,8 @@ import { UsuarioService } from 'src/app/service/Usuario/usuario.service';
 })
 export class ListarMultasComponent {
   multas:Multa[] = [];
+  multaEmEdicao:any | null = null;
+  editMode:boolean[]=[];
 
 
   @Input() multa: Multa = new Multa();
@@ -33,6 +35,19 @@ export class ListarMultasComponent {
         console.error(erro);
       }
     )
+  }
+  editarItem(index:number ) {
+    this.multaEmEdicao = {...this.multas[index] };
+    this.editMode[index] = true;
+  }
+  salvarEdicao(index:number) {
+    this.multas[index] = { ...this.multaEmEdicao };
+    this.editMode[index] = false;
+    this.multaEmEdicao = null;
+  }
+  cancelarEdicao(index: number) {
+    this.editMode[index] = false;
+    this.multaEmEdicao = null;
   }
 
 
